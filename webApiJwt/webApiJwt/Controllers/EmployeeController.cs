7f38e0b7b782f516743d7ce34329c5c701ed93d0
shahinda10 <127094511+shahinda10@users.noreply.github.com>
2023-03-31 14:36:36 +0530

    Add files via upload

---
+++ webApiJwt/webApiJwt/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiJwt.Models;
+
+namespace webApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+
+        public string GetData()
+        {
+            return "authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+
+        public string Details()
+        {
+            return "authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+       
+
+        public string AddUser(Users user)
+        {
+            return "user added with username"+ user.username;
+        }
+
+
+    }
+}
---
+++ webApiJwt/webApiJwt/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using webApiJwt.Models;
+
+namespace webApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _config= configuration;
+        }
+         private Users Authentication(Users user)
+        {
+            Users _user = null;
+        
+            if (user.username == "admin" && user.password == "1234")
+            {
+                _user = new Users { username = "shahinda" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var crentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],null,
+                expires:DateTime.Now.AddMinutes(1),
+                signingCredentials:crentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = Authentication(user);
+            if(user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+
+            }
+            return response;
+        }
+    }
+}
